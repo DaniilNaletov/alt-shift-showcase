62c4525d97870c48e26f19c83276c94a77b49ca6
DaniilNaletov <daniilnaletov@gmail.com>
2025-10-30 21:08:20 +0100

    Add react-router

--- src/app/Root.tsx
+++ src/app/Root.tsx
@@ -1,13 +1,37 @@
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom'
+
 import HomePage from '@/pages/home/HomePage'
+import TestUIPage from '@/pages/test-ui/TestUIPage'
 
 import AppLayout from './AppLayout'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <AppLayout>
+        <Outlet />
+      </AppLayout>
+    ),
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: '/create',
+        element: <div>Create Page</div>,
+      },
+    ],
+  },
+  {
+    path: '/test-ui',
+    element: <TestUIPage />,
+  },
+])
+
 const Root: React.FC = () => {
-  return (
-    <AppLayout>
-      <HomePage />
-    </AppLayout>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default Root
