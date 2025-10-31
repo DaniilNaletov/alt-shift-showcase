e5d4d8c3c6edc459d91b590e391d114154b17e1f
DaniilNaletov <daniilnaletov@gmail.com>
2025-10-31 13:50:32 +0100

    Altui: <AltLogo /> add minimal version

--- src/altui/components/AltLogo/AltLogo.tsx
+++ src/altui/components/AltLogo/AltLogo.tsx
@@ -1,13 +1,11 @@
 import { ReactComponent as Logo } from './logo.svg'
+import { ReactComponent as LogoMinimal } from './logo-minimal.svg'
 
 const AltLogo: React.FC<{
   className?: string
-}> = ({ className }) => {
-  return (
-    <div className={className}>
-      <Logo />
-    </div>
-  )
+  minimal?: boolean
+}> = ({ className, minimal }) => {
+  return <div className={className}>{minimal ? <LogoMinimal /> : <Logo />}</div>
 }
 
 export default AltLogo
